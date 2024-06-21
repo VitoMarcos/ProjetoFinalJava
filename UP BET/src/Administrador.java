279868daebdab55c07c5dcc2a55b95a7131e1e0f
VitoMarcos <vitormghansen@gmail.com>
2024-06-21 09:40:01 -0300

    Primeiro commit, criação do projeto Java e das classes de usuário e administrador

---
+++ UP BET/src/Administrador.java
@@ -0,0 +1,29 @@
+public class Administrador extends Pessoa {
+    
+    public Administrador(int id, String nome, String cpf, String email){
+        super(id, nome, cpf, email);
+    }
+
+    
+    
+    @Override
+    public String exibirDados() {
+        
+        return "Administrador\nId: " + getId() + "Nome: " + getNome() + "\nCPF: " + getCpf() + "\nEmail: " + getEmail();
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Administrador\\n" + //
+                        "Id: \" + getId() + \"Nome: \" + getNome() + \"\\n" + //
+                        "CPF: \" + getCpf() + \"\\n" + //
+                        "Email: \" + getEmail();";
+    }
+
+    
+
+
+
+}
